8e6b082a157db21b30fd15e54b77aeb3d5bbc97a
hecj <275070023@qq.com>
2014-10-28 14:48:22 +0800

    udpate

--- src/main/java/com/hecj/web/action/TestAction.java
+++ src/main/java/com/hecj/web/action/TestAction.java
@@ -1,5 +1,7 @@
 package com.hecj.web.action;
 
+import com.hecj.dao.dao.StudentDao;
+import com.hecj.dao.dao.imp.StudentDaoImp;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class TestAction extends ActionSupport {
@@ -9,10 +11,21 @@ public class TestAction extends ActionSupport {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private StudentDao studentDao;
 	
+	public StudentDao getStudentDao() {
+		return studentDao;
+	}
+
+	public void setStudentDao(StudentDao studentDao) {
+		this.studentDao = studentDao;
+	}
+
 	public String test() {
 		
 		System.out.println("进来了...");
+		
+		System.out.println(studentDao);
 		
 		return "success" ;
 	}
